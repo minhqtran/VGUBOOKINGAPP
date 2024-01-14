369c389516a63351da312f5843664edc1df1d395
Hai Cao Xuan <hia.cao.2001@gmail.com>
2024-01-15 02:49:55 +0700

    navigation bar

--- src/components/sidenav/Sidenav.tsx
+++ src/components/sidenav/Sidenav.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Layout, Menu } from 'antd';
+import { Layout, Menu, ConfigProvider } from 'antd';
 import {
   HomeOutlined,
   CalendarOutlined,
@@ -16,37 +16,70 @@ import {
   LogoutOutlined,
 } from '@ant-design/icons';
 import { Link } from 'react-router-dom';
-import VGULogo from '../../assets/img/VGU-logo.png'
+import VGULogo from '../../assets/img/VGU-logo2.png'
 import './sidenav.css'
 import 'boxicons'
+
 
 const { Sider } = Layout;
 
 const Sidenav: React.FC = () => {
   return (
-    <Sider width={250} theme="light" breakpoint="lg" collapsedWidth="0">
-        <div className="sidenav-logo-name-container">
-        <img src={VGULogo} alt="Logo" className="sidenav-logo" />
-        <div className="sidenav-app-name">VGU Booking App</div>
+    <ConfigProvider
+    theme={{
+      token: {
+        // Seed Token
+        colorPrimary: '#F28130',
+        borderRadius: 5,
+
+        // Alias Token
+        
+      },
+    }}
+  >
+    <Sider className='sider-container' width={250} theme="light" breakpoint="lg" collapsedWidth="0">
+        <div className="sider-logo-name-container">
+          <img src={VGULogo} alt="Logo" className="sider-logo" />
+          <div className="sider-app-name">VGU Booking App</div>
         </div>
         <Menu 
             mode="vertical" 
             theme="light" 
             defaultSelectedKeys={['1']}
-            style={{ height: '100vh'}}
+            className='sider-menu-bar'
         >
         {/* Booking Section */}
-        <Menu.SubMenu key="booking" icon={<BookOutlined />} title="Booking">
-          <Menu.Item key="roomBooking">
-            <Link to="/room-booking">Room Booking</Link>
+        <Menu.SubMenu  
+          key="booking" 
+          icon={<BookOutlined />} 
+          title="Booking"
+        >
+          <Menu.Item 
+            key="roomBooking" 
+            
+          >
+            <Link 
+              to="/room-booking"
+            >
+              Room Booking
+            </Link>
           </Menu.Item>
           <Menu.Item key="vehicleBooking">
-            <Link to="/vehicle-booking">Vehicle Booking</Link>
+            <Link 
+              to="/vehicle-booking"
+            >
+              Vehicle Booking
+              </Link>
           </Menu.Item>
         </Menu.SubMenu>
 
         {/* My Booking Section */}
-        <Menu.SubMenu key="myBooking" icon={<UnorderedListOutlined />} title="My Booking List">
+        <Menu.SubMenu 
+          key="myBooking" 
+          icon={<UnorderedListOutlined />} 
+          title="My Booking List"
+          className='sider-menu-submenu'
+        >
           <Menu.Item key="roomBookingList">
             <Link to="/room-booking-list">Room Booking List</Link>
           </Menu.Item>
@@ -94,6 +127,7 @@ const Sidenav: React.FC = () => {
       </div>
       </Menu>
     </Sider>
+    </ConfigProvider>
   );
 };
 
