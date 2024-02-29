ef2f3e05f1b3123e8011f1af1aaece72582bf873
Quynh.BH <quynh.bh@vgu.edu.vn>
2024-02-29 15:14:33 +0700

    #fix restore

--- API/obj/Debug/net5.0/BookingApp.AssemblyInfo.cs
+++ API/obj/Debug/net5.0/BookingApp.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookingApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef44ea8a123e4443b36d14428ff35cff6c5ce230")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c0835fb90efe8ffb1f7673b6cb54becc77f45aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookingApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookingApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
