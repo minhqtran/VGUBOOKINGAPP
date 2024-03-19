893c11591396fbc6a7c445bfecf2a597480a6433
LoveLess <116763405+LoveLess1212@users.noreply.github.com>
2024-03-19 13:46:28 +0700

    add gitignore

--- API/DTO/Filter/UserFilter.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BookingApp.DTO.Filter
-{
-    public class UserFilter
-    {
-        public string Department { get; set; }
-        public int Role { get; set; }
-        public bool LDapLogin { get; set; }
-    }
-}
